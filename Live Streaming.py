3680bb31057219bce4e00ef00f6401f3da391185
aftabh4004 <57296741+aftabh4004@users.noreply.github.com>
2020-05-19 22:21:27 +0530

    Final Commit, Development done locally

---
+++ Live Streaming.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Live Streaming
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+import webbrowser
+import os
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def face_detector(img, size=0.5):
+    
+    faces = face_classifier.detectMultiScale(img, 1.3, 5)
+    if faces is ():
+        return img, []
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (244, 224))
+    return img, roi
+
+
+model = load_model('vgg_face_recog.h5')
+# Open Webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+    
+    image, face = face_detector(frame)
+    
+    face = np.expand_dims(face, axis=0)
+    #print(face.shape)
+    try:
+        # Pass face to prediction model
+        # "results" comprises of a tuple containing the label and the confidence value
+        res = np.argmax(model.predict(face, 1, verbose = 0), axis=1)
+        print(res)
+        if res[0] == 0:
+            cv2.putText(image, "Hey Aftab", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+            cv2.imshow('Face Recognition', image )
+        elif res[0] == 1:
+            cv2.putText(image, "Hey Kushagra", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+            cv2.imshow('Face Recognition', image )        
+       
+
+    except:
+        cv2.putText(image, "No Face Found", (220, 120) , cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 2)
+        cv2.imshow('Face Recognition', image )
+        pass
+        
+    if cv2.waitKey(1) == 13: #13 is the Enter Key
+        break
+        
+cap.release()
+cv2.destroyAllWindows()   
+
---
+++ VGG face Recog Transfer Learning.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from keras.applications import VGG16
+
+
+# In[2]:
+
+
+#downloading VGG16 weight
+vggmodel = VGG16(weights = 'imagenet', include_top = False, input_shape = (224, 244, 3) )
+
+
+# In[3]:
+
+
+#freezing all layer
+for layer in vggmodel.layers:
+    layer.trainable = False
+
+
+# # creating Fully connected layers
+
+# In[5]:
+
+
+def FClayer(model):
+    newmodel = model.output
+    newmodel = Flatten()(newmodel)
+    newmodel = Dense(units = 256, activation = 'relu')(newmodel)
+    newmodel = Dense(units = 128, activation = 'relu')(newmodel)
+    newmodel = Dense(units = 32, activation = 'relu')(newmodel)
+    newmodel = Dense(units = 2, activation = 'softmax')(newmodel)
+    return newmodel
+
+
+# # Adding FC onto VGG
+
+# In[6]:
+
+
+from keras.layers import Dense, Flatten
+from keras.models import Model
+
+head = FClayer(vggmodel)
+
+vggmodel = Model(inputs = vggmodel.input, outputs = head)
+
+vggmodel.summary()
+
+
+# ## Compiling model
+
+# In[7]:
+
+
+from keras.optimizers import RMSprop
+vggmodel.compile(loss = 'categorical_crossentropy',
+              optimizer = RMSprop(lr = 0.001),
+              metrics = ['accuracy'])
+
+
+# ## Augmenting Images and traing the model
+
+# In[9]:
+
+
+from keras_preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        './aftab/training/',
+        target_size=(224, 244),
+        class_mode='categorical')
+test_set = test_datagen.flow_from_directory(
+        './aftab/test/',
+        target_size=(224, 244),
+        class_mode='categorical')
+vggmodel.fit(
+        training_set,
+        epochs=5,
+        validation_data=test_set,
+        )
+
+vggmodel.save('vgg_face_recog.h5')
+# In[11]:
+
+
+test_set.class_indices
+
+
+# In[10]:
+
+
+vggmodel.save('vgg_face_recog.h5')
+
+
+# In[ ]:
+
+
+
+
